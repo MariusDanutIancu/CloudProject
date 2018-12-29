8dea1af347745dea14ab6ffb7156f0c724c54e18
Marius Danut Iancu <MariusIancu@tss-yonder.com>
2018-12-29 23:16:56 +0200

    (EUREKA) Configured eureka admin service for cluster

--- eureka/admin-service/src/main/java/com/cloud/adminservice/AdminServiceApplication.java
+++ eureka/admin-service/src/main/java/com/cloud/adminservice/AdminServiceApplication.java
@@ -1,18 +1,18 @@
-package com.cloud.gatewayservice;
+package com.cloud.adminservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
-@EnableDiscoveryClient
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication
-public class GatewayServiceApplication {
+@EnableDiscoveryClient
+@EnableAdminServer
+public class AdminServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
+		SpringApplication.run(AdminServiceApplication.class, args);
 	}
 
 }
-
