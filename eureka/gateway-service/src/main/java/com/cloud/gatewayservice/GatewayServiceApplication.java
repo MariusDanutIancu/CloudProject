3c2a0156fdadae83629bda141b34780aa779c7b0
Marius Danut Iancu <MariusIancu@tss-yonder.com>
2018-12-30 10:22:23 +0200

    (EUREKA) Integrated swagger2

--- eureka/gateway-service/src/main/java/com/cloud/gatewayservice/GatewayServiceApplication.java
+++ eureka/gateway-service/src/main/java/com/cloud/gatewayservice/GatewayServiceApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableZuulProxy
+@EnableSwagger2
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -13,6 +16,6 @@ public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
-
+		
 }
 
--- eureka/protected-service/src/main/java/com/cloud/protectedservice/ProtectedServiceApplication.java
+++ eureka/protected-service/src/main/java/com/cloud/protectedservice/ProtectedServiceApplication.java
@@ -1,15 +1,48 @@
 package com.cloud.protectedservice;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
-@EnableEurekaClient
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableDiscoveryClient
+@EnableSwagger2
 @SpringBootApplication
 public class ProtectedServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProtectedServiceApplication.class, args);
+	}
+	
+	@Bean
+	public Docket api() throws IOException, XmlPullParserException {
+		MavenXpp3Reader reader = new MavenXpp3Reader();
+		Model model = reader.read(new FileReader("pom.xml"));
+		Collection<VendorExtension> vendorExtesions = new ArrayList<VendorExtension>();
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.cloud.protectedservice"))
+				.paths(PathSelectors.any())
+				.build().apiInfo(new ApiInfo("Account Service Api Documentation", "Documentation automatically generated", model.getParent().getVersion(), null, new Contact("Marius Iancu", "-", "marius.danut94@gmail.com"), null, null, vendorExtesions));
 	}
 }
 
